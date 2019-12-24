ec5a30e238eff18d37687503d363b67ca583b3d4
Danilo Alonso <dalonso@miamidevshop.io>
2019-12-24 03:54:58 -0500

    typescript only types, fix refetch, improved intellisense

--- __tests__/index.js
+++ __tests__/index.js
@@ -27,7 +27,6 @@ describe('saga slice helpers', () => {
                 'noop',
                 'crudInitialState',
                 'crudReducers',
-                'lifecycleReducers',
                 'createApis'
             ])
         );
